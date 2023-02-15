5650128b46a0226a723330f1eac997bf414e49fb
haolinggithub <47423903+haolinggithub@users.noreply.github.com>
2023-02-15 21:49:22 +0000

    Feature (#3)
    * new file added
    
    * add comment
    
    * add print

---
+++ t1.py
@@ -0,0 +1,25 @@
+# this is test for git pull request
+def solution(S):
+    # write your code in Python 3.8.10
+
+    result = list(S)
+    for i in range(len(S)):
+        l = i
+        r = len(S)-1-i
+        if l>=r:
+            if l==r and S[l]=='?':
+                result[l]='a'
+            return ''.join(result)
+        if S[l]!='?' and S[r]!='?' and S[l]!=S[r]:
+            return 'NO'
+        if S[l]=='?' and S[r]=='?':
+            result[l], result[r] = 'a', 'a'
+        elif S[l]=='?':
+            result[l] = result[r]
+        elif S[r]=='?':
+            result[r] = result[l]
+
+
+
+print(solution("?ab??a"))
+print('done')
